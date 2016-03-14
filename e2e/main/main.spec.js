39c80f51625f4e086df3d1b9ab23954a30bc8e91
Sunil B <sbandla@apexclearing.com>
2016-03-14 10:30:28 -0500

    add e2e

---
+++ e2e/main/main.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var config = browser.params;
+
+describe('Main View', function() {
+  var page;
+
+  beforeEach(function() {
+    browser.get(config.baseUrl + '/');
+    page = require('./main.po');
+  });
+
+});
